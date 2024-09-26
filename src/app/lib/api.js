d058b9d9074ebe4aaed3707603b18a8aa7f9bd7c
PSjoon <pedrosantos.joon@gmail.com>
2024-09-26 20:55:42 -0300

    fix(Register): adiciona sistema de login em deploy

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -3,3 +3,7 @@ import axios from 'axios'
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 })
+
+// export const api = axios.create({
+//   baseURL: 'https://back-locadora-red-devs.onrender.com/Register', @Viana-Gabriel
+// })
