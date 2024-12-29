68135fa0242b217292ae6a143b11e5426e6b84d2
PSjoon <pedrosantos.joon@gmail.com>
2024-12-29 14:34:11 -0300

    fix: LinkNavigation Link

--- src/components/Navagation/LinkNavigation/index.jsx
+++ src/components/Navagation/LinkNavigation/index.jsx
@@ -1,15 +1,14 @@
-import DropBox from '@/components/DropBox';
+import DropBox from '@/components/DropBox'
 import Image from 'next/image'
-import Link from 'next/link';
+import Link from 'next/link'
 import React, { useState } from 'react'
 
 const LinkNavigation = ({ titulo, src, alt, index }) => {
-  const [openDropBox, setOpenDropBox] = useState(null);
+  const [openDropBox, setOpenDropBox] = useState(null)
 
   const handleDropBoxToggle = (index) => {
-    setOpenDropBox(prevIndex => (prevIndex === index ? null : index));
-  };
-
+    setOpenDropBox((prevIndex) => (prevIndex === index ? null : index))
+  }
 
   /* Posso abstrair esses arrays e colococar num arquico a parte e chamalos para deixar o codigo mais limpo */
 
@@ -18,33 +17,32 @@ const LinkNavigation = ({ titulo, src, alt, index }) => {
     'Drama',
     'Comédia romântica',
     'Ficção científica',
-    'Terror'
+    'Terror',
   ]
-  const decadas = [
-    '10 a 30',
-    '30 a 40',
-    '50 a 60',
-    '70 a 80',
-    '90 a 2000'
-  ]
+  const decadas = ['10 a 30', '30 a 40', '50 a 60', '70 a 80', '90 a 2000']
 
   return (
     <>
-      <Link href={''}> {/* Link pode ser passado como props, ou utilizar '/${item}' */}
-        <li className=' px-4 border-b-2 h-[80px] flex items-center gap-2' onClick={() => handleDropBoxToggle(index)}>
-          <Image
-            src={src}
-            height={25}
-            width={25}
-            alt={alt}
-          />
+      <Link href={''}>
+        {' '}
+        {/* Link pode ser passado como props, ou utilizar '/${item}' */}
+        <li
+          className=' px-4 border-b-2 h-[80px] flex items-center gap-2'
+          onClick={() => handleDropBoxToggle(index)}>
+          <Image src={src} height={25} width={25} alt={alt} />
           {titulo}
         </li>
       </Link>
-      {index === 2 || index === 3 ? <DropBox lista={index === 2 ? generos : decadas} open={openDropBox === index} /> : ''}
+      {index === 2 || index === 3 ? (
+        <DropBox
+          lista={index === 2 ? generos : decadas}
+          open={openDropBox === index}
+        />
+      ) : (
+        ''
+      )}
     </>
   )
-
 }
 
 export default LinkNavigation
--- src/components/Navagation/index.jsx
+++ src/components/Navagation/index.jsx
@@ -1,64 +1,57 @@
 import Image from 'next/image'
 import React, { useState } from 'react'
-import LinkNavigation from './LinkNavigation';
+import LinkNavigation from './LinkNavigation'
 
 const Navegation = ({ isOpen, setIsOpen }) => {
-
   const linksNavigation = [
     {
       titulo: 'Carrinho',
       src: '/carrinho.svg',
-      alt:'carrinho'
+      alt: 'carrinho',
     },
     {
       titulo: 'Décadas',
       src: '/film.svg',
-      alt:'filme'
+      alt: 'filme',
     },
     {
       titulo: 'Gêneros',
       src: '/film.svg',
-      alt:'filme'
+      alt: 'filme',
     },
     {
       titulo: 'Locadoras',
       src: '/sacola.svg',
-      alt:'sacola'
+      alt: 'sacola',
     },
     {
       titulo: 'Sobre nós',
       src: '/sobre.svg',
-      alt:'livro'
+      alt: 'livro',
     },
-
   ]
 
   return (
     <>
-      <div className='top-0 z-[1000] fixed h-full w-full bg-slate-600 opacity-70 cursor-pointer' onClick={() => setIsOpen(!isOpen)} ></div>
+      <div
+        className='top-0 z-[1000] fixed h-full w-full bg-slate-600 opacity-70 cursor-pointer'
+        onClick={() => setIsOpen(!isOpen)}></div>
       <div className='overflow-y-auto h-full w-[70vw] max-w-96 bg-destaque fixed z-[1000] top-0 '>
         <div className='sticky top-0 flex flex-col justify-end h-[20%] bg-[url("/bg_navegacao_mobile.png")] bg-cover bg-no-repeat'>
           <div className='flex items-center gap-2 relative '>
             <div className='m-2 border-2 rounded-full'>
-              <Image
-                src={'/person.png'}
-                height={67}
-                width={54}
-                alt='perfil'
-              />
+              <Image src={'/person.png'} height={67} width={54} alt='perfil' />
             </div>
             <p>Username</p>
           </div>
-
         </div>
         <ul>
-          {linksNavigation.map((link,index)=>(
-            <LinkNavigation {...link} index={index + 1}/>
+          {linksNavigation.map((link, index) => (
+            <LinkNavigation key={link.titulo} {...link} index={index + 1} />
           ))}
         </ul>
       </div>
     </>
-
   )
 }
 
